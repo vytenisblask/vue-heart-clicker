9e6cc354a23de794dce5730d24ffbe1afa5241c0
vytenisblask <vytenisb@yahoo.com>
2023-09-12 12:14:30 +0300

    heart animation update

--- src/components/HeartClicker.vue
+++ src/components/HeartClicker.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="container">
-    <div 
-      class="heart" 
-      :class="{ 'clicked': isClicked }" 
-      @click="toggleHeart">
-    </div>
-    <div class="heart-counter" v-if="clickedCount">
+    <ToggleFavorite 
+      @clicked="toggleHeart" 
+      :favorited="isClicked"
+    />
+    <div class="heart-counter" v-if="isClicked">
       + {{ clickedCount }}
     </div>
   </div>
@@ -13,8 +12,12 @@
 
 <script>
 import axios from 'axios';
+import ToggleFavorite from './ToggleFavorite.vue';
 
 export default {
+  components: {
+    ToggleFavorite
+  },
   data() {
     return {
       isClicked: false,
@@ -49,7 +52,6 @@ export default {
 }
 </script>
 
-
 <style scoped>
 .container {
   margin: 0;
@@ -61,59 +63,12 @@ export default {
   background-repeat: no-repeat;
   background-size: contain;
 }
-.heart {
-  position: absolute;
-  bottom: 1rem;
-  right: 4rem;
-  width: 50px;
-  height: 50px;
-  cursor: pointer;
-  background-size: contain;
-}
-
-/* Initial state - outline heart */
-.heart::before {
-  content: "";
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: url("~@/assets/np_heart_outline.svg") no-repeat center;
-  background-size: contain;
-  opacity: 1;
-  transition: opacity 0.3s ease-in-out;
-}
-
-/* Clicked state - full red heart */
-.heart::after {
-  content: "";
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: url("~@/assets/np_heart_red.svg") no-repeat center;
-  background-size: contain;
-  opacity: 0;
-  transition: opacity 0.3s ease-in-out;
-}
-
-/* When heart is clicked, toggle opacity */
-.heart.clicked::before {
-  opacity: 0;
-}
-
-.heart.clicked::after {
-  opacity: 1;
-}
-
 .heart-counter {
   font-family: 'Montserrat', sans-serif;
   font-family: 'Roboto', sans-serif;
   position: absolute;
-  bottom: 1.6rem;
-  right: 1rem;
+  top: 1.6rem;
+  left: 4rem;
   font-size: 20px;
   color: rgb(18, 16, 16);
 }
